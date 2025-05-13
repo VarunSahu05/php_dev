6cc9640a27fc639c8926d282b85f6c6e85b715e6
VarunSahu05 <varunsahu909090@gmail.com>
2025-05-13 21:25:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PHP Development Environment
+
+This project is set up for local PHP and MySQL development.
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Hello, World!";
+    echo "Welcome to my PHP script.";
+    echo "This is a simple demonstration of PHP.";
+    // THIS IS A COMMENT
+?>
